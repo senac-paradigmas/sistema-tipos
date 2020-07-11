1a9e6cf6590cd2823402557cacf6ce1b37e434b3
Celso Crivelaro <celsocrivelaro@gmail.com>
2020-07-11 10:35:18 -0300

    Limites e Tipagem forte ou fraca

---
+++ forte-fraca/forte.py
@@ -0,0 +1,10 @@
+i = 10 # inteiro
+s = "Oi!" # string
+
+print(type(i))
+print(type(s))
+
+resultado = i + s # que tipo é resultado?
+
+print(type(resultado))
+print(resultado)
---
+++ forte-fraca/fraca.js
@@ -0,0 +1,10 @@
+i = 10; // inteiro
+s = "Oi!"; // string
+
+console.log(typeof(i));
+console.log(typeof(s));
+
+resultado = i + s; // que tipo é resultado?
+
+console.log(typeof(resultado));
+console.log(resultado);
